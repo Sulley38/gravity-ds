60bf8acfa54100b54af3ce9d7691d8fcb3d6913e
Iván Matellanes <imatellanes003@ikasle.ehu.es>
2012-04-19 11:49:52 +0200

    Temporizadores terminado

--- include/temporizadores.h
+++ include/temporizadores.h
@@ -3,12 +3,13 @@ temporizadores.h
 -------------------------------------*/
 #include <nds.h>
 
-extern int tiempo;
+extern int obtenerTiempo();
+extern void resetearTiempo();
 
 extern void HabilitarIntTemp();
 extern void DeshabilitarIntTemp();
 
-extern void prepararTemporizador(uint8 id, int frecuencia);
-extern void iniciarTemporizador(uint8 id);
-extern void pararTemporizador(uint8 id);
+extern void prepararTemporizador(int frecuencia);
+extern void iniciarTemporizador();
+extern void pararTemporizador();
 extern void intTemporizador();
