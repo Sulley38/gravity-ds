e21d430b5a8d7a4520c9eb4c4ce996e731134f57
Asier Mujika <asier_mujika@hotmail.com>
2012-04-19 11:22:13 +0200

    mi test

---
+++ test/testTeclado.c
@@ -0,0 +1,14 @@
+#include "teclado.h"
+#include <nds.h>
+#include <stdio.h>
+
+int main() {
+
+	consoleDemoInit();
+
+	iprintf( "Chicos! Hoy vamos a aprender a contar hasta 20.\n" );
+	while(TRUE)	{
+
+	}
+	return 0;
+}
