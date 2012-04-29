151cba6bc66d36322d207e536924830bd2960f62
dani <dani_alza_93@hotmail.com>
2012-04-29 14:31:28 +0200

    lo que he hecho el jueves

---
+++ include/pantalla.h
@@ -0,0 +1,12 @@
+#include <nds.h>
+
+//Nos devuelve la posicion de la pantalla en la que se haya tocado la pantalla tactil por encuesta
+//hasta que el usuario toque en algun lugar de la pantalla
+touchPosition posicionPantallaEncuesta();
+
+
+//Nos devuelve la posicion de la pantalla en la que se haya tocado si se ha tocado,
+//en caso contrario nos devolverá lad coordenadas 0,0
+touchPosition posicionPantalla ();
+
+
---
+++ test/testPantalla.c
@@ -0,0 +1,19 @@
+#include <nds.h>
+#include <stdio.h>
+#include "pantalla.h"
+
+void main(){
+
+
+	consoleDemoInit();
+	touchPosition pantalla;
+
+
+
+
+		while(TRUE)	{
+			pantalla = posicionPantalla();
+		iprintf("%d , %d",pantalla.px,pantalla.py);
+	}
+
+}
