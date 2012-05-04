d09ab79b395458eb7904d1b8d1c6216a8806e6da
dani <dani_alza_93@hotmail.com>
2012-05-04 18:01:58 +0200

    Ya he añadido lo de las monedas, ahora falta que se visualizen los puntos arriba, ya pensare como hacerlo. Tambien he añadido una nueva variable global PUNTOS_TOTALES en defines.h

--- test/testGraficos.c
+++ test/testGraficos.c
@@ -5,6 +5,7 @@
 #include "interrupciones.h"
 #include "sprites.h"
 
+int PUNTOS_TOTALES=0;
 int ESTADO = AVANZAR_PERSONAJE;
 int posiciones_moneda[9][3]={{260,151,0}, {290,48,0}, {320,213,0}, {350,208,0}, {380,122,0}, {410,159,0}, {440,208,0}, {470,71,1}, {500,214,1}};
 
@@ -23,18 +24,41 @@ void dibujar_monedas(){
 	}
 }
 
+//Con esta función comprobamos si se ha tocado una moneda de la pantalla
+uint8 tocar(uint8 x, uint8 y){
+
+	uint8 tocado = 0;
+	touchPosition pos_pantalla;
+	touchRead(&pos_pantalla);
+
+if (pos_pantalla.px >= x && pos_pantalla.px <= x+8){
+	if (pos_pantalla.py >= y && pos_pantalla.py <= y+16){
+	tocado=1;}
+}
+return tocado;
+}
+
+
 void limpiar_monedas(){
 	int i;
 	for (i=0;i<9;i++){
-		posiciones_moneda[i][0] -= 5;
+		posiciones_moneda[i][0] -= 1;
 		if (posiciones_moneda[i][0]<-26){
 			posiciones_moneda[i][0] = 270;
 			posiciones_moneda[i][1] = ((posiciones_moneda[i][1]*1397+123)%150)+20;
 			posiciones_moneda[i][2] = 1;
 		}
+		if (tocar(posiciones_moneda[i][0],posiciones_moneda[i][1]) == 1){
+			PUNTOS_TOTALES+= 50;
+			posiciones_moneda[i][2]=0;
+		}
 	}
 
+
 }
+
+
+
 
 int main(){
 
